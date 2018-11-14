c29d922c5b78dd79d9166afe705ae5c69a55985b
Joseph Ferro <joejoebeef@gmail.com>
2018-11-13 17:24:02 -0700

    all CRUD actions work

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,13 +1,32 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
   end
 
   def show
+    @people = Person.find(params[:id])
   end
 
   def new
+    @people = Person.new
   end
 
-  def edit
+  def create
+    @people = Person.new(people_params)
+ 
+    if @people.save
+      redirect_to people_path
+    else
+      render :new
+    end
   end
+ 
+  private
+ 
+    def people_params
+      params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+    end
+  # def edit
+  #   @people = Person.find(params[:id])
+  # end
 end
